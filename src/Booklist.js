4cc8dd09c93778446f592be1a24327d3dfda1b02
Turkiqsm <trk996@gmail.com>
2019-09-30 01:29:58 +0300

    first submet

---
+++ src/Booklist.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react'
+import PropTyps from 'prop-types'
+
+
+class Booklist extends Component{
+    static propTypes = {
+        Books: PropTyps.array.isRequired,
+    }
+
+
+    handleChange = (Book , value)=>{
+        if(this.props.update(Book,value)){
+            this.props.update(Book , value)
+        }
+    }
+render(){
+
+    const {Books,Shelf} = this.props
+    const ShlefBooks = Shelf === '' ? Books : 
+        Books.filter((book) => (
+            book.shelf.includes(Shelf)
+        ))    
+    return(
+
+        <ol className="books-grid">
+        {ShlefBooks.map((Book)=>(
+
+        <li key={Book.id} >
+            <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${Book.imageLinks.thumbnail})` }}></div>
+                <div className="book-shelf-changer">
+                <select
+                onChange={(event) => this.handleChange(Book , event.target.value)}
+                >
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+                </div>
+            </div>
+                <div className="book-title">{Book.title}</div>
+                <div className="book-authors">{Book.authors}</div>
+            </div>
+        </li>
+        ) )}
+        </ol>
+    )
+}
+}
+
+
+
+export default Booklist
---
+++ src/Search.js
@@ -0,0 +1,81 @@
+import React,{Component} from 'react'
+import {Link} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import BooklistSearch from './BooklistSearch.js'
+
+class Search extends Component{
+    state = {
+        query: '',
+        Books: []
+    }
+    updateShelf = (book ,shelf)=>{
+
+        BooksAPI.update(book,shelf)
+        .then((Books)=>{
+          this.setState(()=>({
+            Books
+          }) )
+        })
+      }
+    updateQuery = (query) =>{
+        this.setState(()=> ({
+
+            query: query.trim()
+        }))
+        BooksAPI.search(query)
+        .then((Books)=>{
+          this.setState(()=>({
+            Books
+          }) )
+        })
+    }
+    render(){
+        const {query} = this.state
+
+        return(
+
+            <div className="search-books">
+            <div className="search-books-bar">
+            <Link 
+            to='/'>
+              <button className="close-search">Close</button>
+            </Link>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input 
+                type="text" 
+                placeholder="Search by title or author"
+                value={query}
+                onChange={(event) => this.updateQuery(event.target.value)}
+
+                />
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid">
+              <BooklistSearch
+              Books={this.state.Books}
+              update = {(book , shelf) =>{
+                this.updateShelf(book , shelf)
+              }}
+            />
+              </ol>
+            </div>
+          </div>
+
+
+
+
+        )
+
+    }
+}
+export default Search
